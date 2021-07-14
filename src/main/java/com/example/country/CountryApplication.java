705492163e73bcc91afd24b74c9f4e547083243b
Austin Keiber <adkeiber@gmail.com>
2021-07-14 15:32:07 -0400

    fixed country app

--- src/main/java/com/example/country/CountryApplication.java
+++ src/main/java/com/example/country/CountryApplication.java
@@ -2,12 +2,21 @@ package com.example.country;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CountryApplication {
+public class CountryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountryApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(countryApplicationClass);
+	}
+	
+	private static Class<CountryApplication> countryApplicationClass = CountryApplication.class;
 
 }
